aa9aee7e22a5c01c7d423f5d1bb254e549ac0bb6
Sensol <80695040+SoongGaktal@users.noreply.github.com>
2021-03-16 15:50:27 +0900

    Setup

---
+++ LMSAutoPlayer_UI.py
@@ -0,0 +1,161 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5 import uic
+from mainCrawler import *
+
+#UI파일 연결
+#단, UI파일은 Python 코드 파일과 같은 디렉토리에 위치해야한다.
+form_class = uic.loadUiType('LMS.ui')[0]
+
+class PlayThread(QThread, QObject): #쓰레딩
+    # 시그널 정의
+    signal_AddLogMessage = pyqtSignal(str)
+    signal_SetLectureName = pyqtSignal(str)
+    sinal_StopFunc = pyqtSignal()
+
+    def __init__(self, parent):
+        super().__init__()
+
+        self.main = parent
+
+        self.isRun = False
+        self.links = None
+        self.id = None
+        self.pw = None
+        self.chromeOption_Hide = False
+        self.chromeOption_Mute = False
+        self.powerOffOption = False
+
+    def InitLinkData(self, new_links):          # 강의 링크 정보 저장
+        self.links = new_links
+    
+    def InitUserData(self, user_id, user_pw):   # 유저 정보 저장
+        self.id = user_id
+        self.pw = user_pw
+
+    def InitOption(self, optionName, option):         # 크롬창 숨기기 옵션 저장
+        if optionName == 'Hide':
+            self.chromeOption_Hide = option
+        if optionName == 'Mute':
+            self.chromeOption_Mute = option
+        if optionName == 'PowerOff':
+            self.powerOffOption = option
+
+    def run(self):
+        mainFunc(self)
+
+#화면을 띄우는데 사용되는 Class 선언
+class WindowClass(QMainWindow, form_class) :
+    def __init__(self) :
+        super().__init__()
+
+        # 스레드 선언 및 시그널 연결
+        self.th = PlayThread(self)
+        self.th.signal_AddLogMessage.connect(self.AddLogMessage)
+        self.th.signal_SetLectureName.connect(self.SetLectureName)
+        self.th.sinal_StopFunc.connect(self.StopFunc)
+
+        # UI 커넥트
+        self.setupUi(self)
+        self.Button_Start.clicked.connect(self.StartFunc)
+        self.Button_Stop.clicked.connect(self.StopFunc)
+        self.Button_Login.clicked.connect(self.LoginFunc)
+        self.Button_clearLog.clicked.connect(self.ClearLog)
+        self.checkbox_toggleChromeToHide.stateChanged.connect(self.ToggleChromeToHide)
+        self.checkbox_toggleChromeToMute.stateChanged.connect(self.ToggleChromeToMute)
+        self.checkbox_togglePowerOff.stateChanged.connect(self.TogglePowerOff)
+
+    def StartFunc(self):
+        links = []
+        links.append(self.link0.text())
+        links.append(self.link1.text())
+        links.append(self.link2.text())
+        links.append(self.link3.text())
+        links.append(self.link4.text())
+        links.append(self.link5.text())
+        links.append(self.link6.text())
+        links.append(self.link7.text())
+        links.append(self.link8.text())
+        links.append(self.link9.text())
+
+        links = list(filter(None, links)) #공백인 리스트 제거
+        self.th.InitLinkData(links)       #쓰레드 멤버 변수에 추가
+        
+        for index, link in enumerate(links):
+            self.AddLogMessage(str(index) + '번째 강의 추가 : ' + link)
+        
+        if not self.th.isRun:
+            self.th.isRun = True
+            self.th.start()
+            self.Button_Start.setDisabled(True)
+            self.group_option.setDisabled(True)
+            self.AddLogMessage("프로그램을 시작합니다..")
+
+
+    def StopFunc(self):
+        if self.th.isRun:
+            self.th.isRun = False
+            self.Button_Start.setDisabled(False)
+            self.group_option.setDisabled(False)
+            self.AddLogMessage("프로그램을 종료합니다..")
+
+    def LoginFunc(self):
+        _id = self.input_ID.text()
+        _pw = self.input_PW.text()
+
+        if not _id or not _pw:
+            self.AddLogMessage('로그인 정보가 비어있습니다!')
+        else:
+            self.AddLogMessage('로그인 정보가 성공적으로 입력되었습니다!')
+            self.th.InitUserData(_id, _pw)  #쓰레드 멤버 변수에 추가
+            self.Button_Start.setDisabled(False)
+            self.Button_Stop.setDisabled(False)
+
+    def ToggleChromeToHide(self):
+        flag = bool(self.checkbox_toggleChromeToHide.checkState())
+        self.th.InitOption('Hide', flag)
+        if flag:
+            self.AddLogMessage('크롬 창을 숨긴채 실행합니다.')
+        else:
+            self.AddLogMessage('크롬 창을 보이게 실행합니다.')
+
+    def ToggleChromeToMute(self):
+        flag = bool(self.checkbox_toggleChromeToMute.checkState())
+        self.th.InitOption('Mute', flag)
+        if flag:
+            self.AddLogMessage('크롬 창을 음소거 상태로 실행합니다')
+        else:
+            self.AddLogMessage('크롬 창을 음소거 상태로 실행하지 않습니다')
+
+    def TogglePowerOff(self):
+        flag = bool(self.checkbox_togglePowerOff.checkState())
+        self.th.InitOption('PowerOff', flag)
+        if flag:
+            self.AddLogMessage('마지막에 자동으로 컴퓨터를 종료합니다')
+        else:
+            self.AddLogMessage('마지막에 자동으로 컴퓨터를 종료하지 않습니다')
+
+    def SetLectureName(self, string):
+        self.textBox_currentLecture.setPlainText(string)
+
+    def AddLogMessage(self, string):
+        self.textbox_Log.append(string)
+
+    def ClearLog(self):
+        self.textbox_Log.clear()
+        self.AddLogMessage('로그 청소 완료!')
+
+
+if __name__ == "__main__" :
+    #QApplication : 프로그램을 실행시켜주는 클래스
+    app = QApplication(sys.argv) 
+
+    #WindowClass의 인스턴스 생성
+    myWindow = WindowClass() 
+
+    #프로그램 화면을 보여주는 코드
+    myWindow.show()
+
+    #프로그램을 이벤트루프로 진입시키는(프로그램을 작동시키는) 코드
+    app.exec_()
---
+++ mainCrawler.py
@@ -0,0 +1,215 @@
+import time
+import os
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys                     #send_key에 필요
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait             #웹드라이버 딜레이
+from selenium.webdriver.support import expected_conditions as EC    #예외처리
+from selenium.common.exceptions import TimeoutException             
+
+def NewSleep(self, driver, _second): #sleep을 0.5초 간격으로 검사, 중간에 스레드가 정지되는걸 체크하기 위함
+    currtime = 0
+    while currtime <= _second:
+        if self.isRun == True:
+            time.sleep(0.5)
+            currtime += 0.5
+        else:
+            self.signal_AddLogMessage.emit("스레드가 실행 도중 종료되었습니다!")
+            driver.quit()
+            exit()
+
+def Shutdown(_time):
+    os.system("shutdown -s -t " + str(_time))
+
+def CheckIframe(driver):    #디버그 전용
+    iframes = driver.find_elements_by_tag_name('iframe')
+    print('현재 페이지에 iframe은 %d개가 있습니다.' % len(iframes))
+
+    for i, iframe in enumerate(iframes):
+	    try:
+	    	print('%d번째 iframe 입니다.' % i)
+
+	    	# i 번째 iframe으로 변경합니다.
+	    	driver.switch_to_frame(iframes[i])
+
+	    	# 변경한 iframe 안의 소스를 확인합니다.
+	    	print(driver.page_source)
+
+	    	# 원래 frame으로 돌아옵니다.
+	    	driver.switch_to_default_content()
+	    except:
+	    	# exception이 발생했다면 원래 frame으로 돌아옵니다.
+	    	driver.switch_to_default_content()
+
+	    	# 몇 번째 frame에서 에러가 났었는지 확인합니다.
+	    	print('pass by except : iframes[%d]' % i)
+
+	    	# 다음 for문으로 넘어갑니다.
+	    	pass
+
+
+def WaitForClass_CanBeClicked(driver, delaySec, class_name):
+    wait = WebDriverWait(driver, delaySec)
+    element = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, class_name)))
+
+def WaitForClass_Visible(driver, delaySec, class_name):
+    wait = WebDriverWait(driver, delaySec)
+    wait.until(EC.visibility_of_element_located((By.CLASS_NAME, class_name)))
+
+def WaitForTag_Visible(driver, delaySec, tag_name):
+    wait = WebDriverWait(driver, delaySec)
+    wait.until(EC.visibility_of_element_located((By.TAG_NAME, tag_name)))
+
+
+def Login(self, driver, id, password):
+    NewSleep(self, driver, 1)
+    try:
+        driver.get('https://myclass.ssu.ac.kr/login.php')
+
+        #ID, PW 필드 채우기
+        driver.find_element_by_xpath('//*[@id="input-username"]').send_keys(id)
+        driver.find_element_by_xpath('//*[@id="input-password"]').send_keys(password)
+
+        #로그인 버튼 클릭
+        driver.find_element_by_xpath('//*[@id="region-main"]/div/div/div/div[3]/div[1]/div[2]/form/div[2]/input').click()
+
+        self.signal_AddLogMessage.emit("스마트캠퍼스에 로그인합니다..")
+    except:
+        self.signal_AddLogMessage.emit("웹페이지 로그인 실패, 서버에 문제가 발생했습니다")
+        return
+
+def OpenLecture(self, driver, link):
+    driver.get(link)
+
+    #동영상 재생버튼 클릭을 위해서 iframe 안에 있는 요소를 확인해주어야 함, 확인하는 함수는 CheckIframe
+    #https://dejavuqa.tistory.com/198 참고
+    WaitForTag_Visible(driver, 10, 'iframe')
+    iframes = driver.find_elements_by_tag_name('iframe')
+    driver.switch_to_frame(iframes[0])
+    NewSleep(self, driver, 3)
+    WaitForClass_CanBeClicked(driver, 10, 'ViewerFrm')
+    driver.find_element_by_class_name('ViewerFrm').click() #재생버튼 클릭
+    NewSleep(self, driver, 3)
+    driver.switch_to_default_content()
+
+
+    # 이어서 시청 팝업창 처리
+    try:
+        WebDriverWait(driver, 10).until(EC.alert_is_present(),
+                                       'Timed out waiting for PA creation ' +
+                                       'confirmation popup to appear.')
+        alert = driver.switch_to.alert
+        alert.accept()
+        self.signal_AddLogMessage.emit("시청기록이 있어 이어서 재생합니다. ")
+        self.signal_AddLogMessage.emit("*재생은 전체 영상시간만큼 진행됩니다. 영상이 끝나고도 한동안 다음으로 넘어가지 않을 수 있습니다")
+    except TimeoutException:
+        self.signal_AddLogMessage.emit("시청기록이 없어 처음부터 재생합니다. ")
+
+    self.signal_AddLogMessage.emit("강의를 시작합니다")
+    lectureName = driver.find_element_by_xpath('//*[@id="vod_header"]/h1').text
+    self.signal_AddLogMessage.emit("강의 이름 : " + str(lectureName))
+    self.signal_SetLectureName.emit(str(lectureName))
+
+def CloseLecture(self, driver):
+    self.signal_AddLogMessage.emit('강의를 종료합니다')
+    driver.execute_script("window.open('');")
+
+    window_to_close = driver.window_handles[0]
+    window_to_open = driver.window_handles[1]
+
+    driver.switch_to.window(window_name=window_to_close)
+    driver.close()
+    driver.switch_to.window(window_name=window_to_open)
+
+def GetLecturePlaytime(self, driver):
+    NewSleep(self, driver, 5)
+    #time.sleep(5)
+    playtime = driver.find_element_by_class_name('playtime').text
+    self.signal_AddLogMessage.emit("현재 강의의 출석인정 수강시간 : " + str(playtime))
+    
+
+    hour = 0
+    minute = 0
+    second = 0
+
+    splitTime = playtime.split(':')
+    count = playtime.count(':')
+
+    if count == 2:  #1시간 이상 강의
+        hour = int(splitTime[0])
+        minute = int(splitTime[1])
+        second = int(splitTime[2])
+    elif count == 1:    #1시간 미만 강의
+        minute = int(splitTime[0])
+        second = int(splitTime[1])
+    #print(count, hour, minute, second)
+
+
+    totalSecond = hour * 3600 + minute * 60 + second
+    self.signal_AddLogMessage.emit("초로 환산 : " + str(totalSecond) + "초")
+
+    return totalSecond
+
+def mainFunc(self):
+    #Initialize
+    links = self.links
+    user_id = self.id
+    user_pw = self.pw
+
+    # =====드라이버 및 옵션 생성=====
+    options = webdriver.ChromeOptions()
+
+    # 필요없고 해결방법도 없는 에러로그들 제거 옵션 추가
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
+    # 창 숨기는 옵션 추가
+    if self.chromeOption_Hide:
+        options.add_argument("headless")
+        self.signal_AddLogMessage.emit("크롬 창을 숨긴 채 진행합니다...")
+
+    # 음소거 옵션 추가
+    if self.chromeOption_Mute:
+        options.add_argument("--mute-audio")
+        self.signal_AddLogMessage.emit("크롬 창을 음소거 상태로 진행합니다...")
+
+    try:
+        driver = webdriver.Chrome('.\chromedriver\chromedriver.exe', options=options)
+    except:
+        self.signal_AddLogMessage.emit("! 크롬 드라이버 로드 실패. 최신버전의 크롬이 설치되어 있는지, chromedriver.exe가 폴더 내에 있는지 확인해주세요.")
+        return;
+
+    # =====로그인=====
+    try:
+        Login(self, driver, user_id, user_pw)
+        #ID, 비밀번호는 사용 직후 초기화
+        user_id = None
+        user_pw = None
+    except:
+        self.signal_AddLogMessage.emit("! 로그인에 실패하였습니다")
+        self.sinal_StopFunc.emit()
+        return
+
+    # =====강의 링크 로드=====
+    if not links:
+        self.signal_AddLogMessage.emit("! 강의 링크가 없습니다. 강의 영상 상단의 링크를 복사해주세요.")
+        self.sinal_StopFunc.emit()
+    else:
+        lectureLinks = links
+        try:
+            for link in links:
+                OpenLecture(self, driver, link)
+                NewSleep(self, driver, GetLecturePlaytime(self, driver))
+                # time.sleep(GetLecturePlaytime(self, driver))
+                CloseLecture(self, driver)
+
+            self.signal_AddLogMessage.emit("강의를 모두 재생했습니다!")
+            self.signal_SetLectureName.emit("실행중인 강의 없음 (모두 재생함)")
+
+            if self.powerOffOption:
+                Shutdown(300)   #5분 뒤 컴퓨터 자동종료
+                self.signal_AddLogMessage.emit("5분 뒤 컴퓨터가 종료됩니다")
+
+        except:
+            self.signal_AddLogMessage.emit("! 강의 로드에 실패했습니다. 로그인 정보와 강의 링크를 확인해주세요")
+            self.sinal_StopFunc.emit()
+    driver.quit()        
